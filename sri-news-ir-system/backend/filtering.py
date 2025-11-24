baeb6e54a414c81376d6848b2757fa037bfb821e
JHOSHETVEGA <84822180+JHOSHETVEGA@users.noreply.github.com>
2025-11-24 11:24:06 -0500

    Update filtering.py

--- sri-news-ir-system/backend/filtering.py
+++ sri-news-ir-system/backend/filtering.py
@@ -1,1 +1,5 @@
-print('filtering placeholder')
+def filter_real_news(model, vectorizer, test_df):
+    X = vectorizer.transform(test_df["text"])
+    test_df["prediction"] = model.predict(X)
+    real_df = test_df[test_df["prediction"] == 1].reset_index(drop=True)
+    return real_df
