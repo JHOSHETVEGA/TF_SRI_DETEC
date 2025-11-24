14bfa183be1cf1d108d5924bf8c9a7c02ecae560
JHOSHETVEGA <84822180+JHOSHETVEGA@users.noreply.github.com>
2025-11-24 11:29:59 -0500

    Update model_training.py

--- sri-news-ir-system/backend/model_training.py
+++ sri-news-ir-system/backend/model_training.py
@@ -5,8 +5,8 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 
 def train_truth_model(train_df, val_df):
     vectorizer = CountVectorizer(stop_words="english")
-    X_train = vectorizer.fit_transform(train_df["text"])
-    X_val   = vectorizer.transform(val_df["text"])
+    X_train = vectorizer.fit_transform(train_df["tweet"])
+    X_val   = vectorizer.transform(val_df["tweet"])
 
     model = MultinomialNB()
     model.fit(X_train, train_df["label"])
@@ -14,9 +14,9 @@ def train_truth_model(train_df, val_df):
     preds = model.predict(X_val)
 
     metrics = {
-        "accuracy": accuracy_score(val_df["label"], preds),
-        "confusion": confusion_matrix(val_df["label"], preds),
-        "report": classification_report(val_df["label"], preds)
+        "accuracy": accuracy_score(val_df["tweet"], preds),
+        "confusion": confusion_matrix(val_df["tweet"], preds),
+        "report": classification_report(val_df["tweet"], preds)
     }
 
     return model, vectorizer, metrics
