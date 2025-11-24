e11ca3c9630cf868779895782fa09699f0caa550
JHOSHETVEGA <84822180+JHOSHETVEGA@users.noreply.github.com>
2025-11-24 15:30:56 -0500

    Delete sri-news-ir-system/backend/filtering.py

--- sri-news-ir-system/backend/filtering.py
+++
@@ -1,6 +0,0 @@
-def filter_real_news(model, vectorizer, test_df):
-    X = vectorizer.transform(test_df["tweet"])
-    test_df["prediction"] = model.predict(X)
-
-    real_df = test_df[test_df["prediction"] == 1].reset_index(drop=True)
-    return real_df
