cd1c1d8f03324da505118bb497b2164e5b3f6352
JHOSHETVEGA <84822180+JHOSHETVEGA@users.noreply.github.com>
2025-11-24 11:48:31 -0500

    Update indexing.py

--- sri-news-ir-system/backend/indexing.py
+++ sri-news-ir-system/backend/indexing.py
@@ -2,10 +2,12 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from rank_bm25 import BM25Okapi
 
+
 def build_tfidf_index(corpus):
     tfidf = TfidfVectorizer(stop_words="english")
     matrix = tfidf.fit_transform(corpus["tweet"])
     return tfidf, matrix
+
 
 def build_bm25_index(corpus):
     tokenized = [doc.split() for doc in corpus["tweet"]]
