214221d10370120a6f2ed5e5a2081e9363dd2bb6
JHOSHETVEGA <84822180+JHOSHETVEGA@users.noreply.github.com>
2025-11-24 11:48:43 -0500

    Update searching.py

--- sri-news-ir-system/backend/searching.py
+++ sri-news-ir-system/backend/searching.py
@@ -6,8 +6,9 @@ def search_tfidf(query, tfidf, tfidf_matrix, corpus, top_k=5):
     top_idx = np.argsort(scores)[::-1][:top_k]
     return corpus.iloc[top_idx], scores[top_idx]
 
+
 def search_bm25(query, bm25, corpus, top_k=5):
-    query_tokens = query.split()
-    scores = bm25.get_scores(query_tokens)
+    tokens = query.split()
+    scores = bm25.get_scores(tokens)
     top_idx = np.argsort(scores)[::-1][:top_k]
     return corpus.iloc[top_idx], scores[top_idx]
